3cfad7d02f4daebd37febcba00abb35925e66248
Cat Ireton <iretoncat@gmail.com>
2024-11-30 19:34:19 -0600

    Add initial create react app - for reals

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Cat's Test Portfolio
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
